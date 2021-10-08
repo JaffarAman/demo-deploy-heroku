5a7cde51fd7f123dc0592a8eeaabdd6308c97276
Jaffar Aman <amanjaffar50@gmail.com>
2021-10-08 15:00:02 +0500

    create request

--- app.js
+++ app.js
@@ -3,6 +3,23 @@ const app = express()
 const PORT = process.env.PORT || 5000;
 
 
+app.get("/profile" , (req,res)=>{
+    res.send("GET PROFILE")
+})
+
+
+app.post("/profile" , (req,res)=>{
+    res.send("Post PROFILE")
+})
+
+app.put("/profile" , (req,res)=>{
+    res.send("UPDATE PROFILE")
+})
+
+app.delete("/profile" , (req,res)=>{
+    res.send("delete PROFILE")
+})
+
 
 
 app.listen(PORT , ()=>console.log(`Server is running on localhost:${PORT}`))
